841e45edf4cc9af70c04bbd9c844d33df7d9e434
EstebanMa12 <desteban.maya@udea.edu.co>
2023-11-29 18:28:39 -0500

    Update settings.py to add login url and make posts views login required

--- Django/posts/views.py
+++ Django/posts/views.py
@@ -1,6 +1,7 @@
 #Django
 from datetime import datetime
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 #Local
@@ -36,6 +37,7 @@ posts = [
 	}
 ]
 # Create your views here.
+@login_required
 def list_posts(request):
     """List existing posts."""
     return render(request, 'posts/feed.html', {'posts':posts})
