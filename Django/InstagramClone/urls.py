dde40407e73c4701ad1cafb5e3ef88ed88f343f0
EstebanMa12 <desteban.maya@udea.edu.co>
2023-11-30 10:47:57 -0500

    Added login, logout, signup views and urls for users app.

--- Django/InstagramClone/urls.py
+++ Django/InstagramClone/urls.py
@@ -32,8 +32,9 @@ urlpatterns = [
     path('sorted/', local_views.sorted, name = 'sort'),
     path('hi/<str:name>/<int:age>/', local_views.say_hi, name =  'hi'),
     path('posts/', posts_views.list_posts, name = 'feed'),
-    path('users/login/', users_views.login_view, name = 'login')
-   
+    path('users/login/', users_views.login_view, name = 'login'),
+    path('users/logout', users_views.logout_view, name = 'logout'),
+    path('users/signup', users_views.signup_view, name = 'signup'),
 ]
 
 if settings.DEBUG:
