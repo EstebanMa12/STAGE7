c116c2867ee6bf2f80960a0b34278c61ff5925e5
EstebanMa12 <desteban.maya@udea.edu.co>
2023-11-30 19:29:33 -0500

    Update signup form and views

--- Django/users/forms.py
+++ Django/users/forms.py
@@ -3,6 +3,9 @@
 from django import forms
 
 # Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
 
 class ProfileForm(forms.Form):
     """Profile form."""
@@ -13,17 +16,49 @@ class ProfileForm(forms.Form):
     
     picture = forms.ImageField()
 
-class SignupForm(forms.Form):
+class SignUpForm(forms.Form):
     """Sign up form"""
-    username = forms.CharField(min_length = 4,max_length = 50)
+    username = forms.CharField(min_length=4, max_length=50)
     
-    password = forms.Charfield(max_length = 70, 
-                            widget = forms.PasswordInput())
-    password_confirmation = forms.Charfield(max_length, 
-                                            widget = forms.PasswordInput())
+    password = forms.CharField(max_length=70, 
+                            widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(max_length=70, 
+                                            widget=forms.PasswordInput())
     
-    first_name = forms.Charfield(min_length = 2, max_length = 50 )
-    last_name = forms.Charfield(min_length = 2, max_length = 50 )
+    first_name = forms.CharField(min_length=2, 
+                                max_length=50)
+    last_name = forms.CharField(min_length=2, 
+                                max_length=50)
     
-    email = forms.Charfield(min_length = 6, max_length = 70, widget = forms.emailInput() )    
+    email = forms.EmailField(min_length=6, 
+                            max_length=70, 
+                            widget=forms.EmailInput())
     
+    def clean_username(self):
+        """Username must be unique"""
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        username_taken = User.objects.filter(username = username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in use')
+        return username
+    
+    def clean(self):
+        """Verify password confirmation match"""
+        data = super().clean()
+        
+        passwd = data['password']
+        passwd_confirmation = data['password_confirmation']
+        
+        if passwd != passwd_confirmation:
+            raise forms.ValidationError('Passwords do not match')
+        return data
+    
+    def save(self):
+        """Create user and profile"""
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+        
+        user = User.objects.create_user(**data)
+        profile = Profile(user=user)
+        profile.save()
--- Django/users/views.py
+++ Django/users/views.py
@@ -1,4 +1,4 @@
-from django.db import IntegrityError
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from users.models import Profile
 
 # Forms 
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignUpForm
 
 
 # Create your views here.
@@ -32,27 +32,18 @@ def logout_view(request):
 def signup_view(request):
     """Signup view"""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignUpForm()
         
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error':'Password confirmation does not match'})
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error':'Username is already in user'})
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        
-        user.save()
-        
-        profile = Profile(user=user)
-        profile.save()
-        
-        return redirect('login')
-    return render(request, 'users/signup.html')
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form':form}
+    )
 
 @login_required
 def update_profile(request):
